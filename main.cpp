46f051fe582b443813ef8806c34634e2c28f7f63
JaeHyeokSeong <seong5763@gmail.com>
2023-02-24 04:36:40 +1100

    4

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 
 int main() {
 	std::cout << "Hello World\n";
+	std::cout << "add\n";
 	std::cout << Calculator::add(3, 4) << '\n';
+	std::cout << "remove\n";
 	std::cout << Calculator::remove(10, 1) << '\n';
 	return 0;
 }
